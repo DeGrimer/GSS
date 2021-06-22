b27b071bb3fa847bc87c3e62fddf9a35f2985909
DeGrimer <danil2609qwe@mail.ru>
2021-06-22 14:42:15 +0400

    Create GSS

---
+++ GSS/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLibrary;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace GSS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var optionsBuilder = new DbContextOptionsBuilder<GoodsContext>();
+
+            var options = optionsBuilder
+                    .UseSqlServer(@"Server=.\SQLEXPRESS;Database=GSSDB;Trusted_Connection=True;")
+                    .Options;
+            using (GoodsContext db = new GoodsContext(options))
+            {
+                var goods = db.goods.ToList();
+                var sales = db.sales.ToList();
+                var departments = db.departments.ToList();
+                //var supplies = db.supplies.ToList();
+            }
+        }
+    }
+}
